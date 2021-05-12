42da97828999c1b39ba585f5eb75ac22d5cf361a
Your Name <you@example.com>
2021-05-12 22:07:12 +0200

    Crating the App and adding the frame
    Installed React in the git repository and added bootstrap and axios. Also cleaned the folders and removed trash.
    
    Added a Welcoming page, with the title "Dictionary" and the search frame and a prelouge.

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className="container">
+
+      <div className="form">
+
+        <form>
+          <input type="search" placeholder="Type here..." />
+          <input type="submit" value="Search" />
+        </form>
+
+      </div>
+
+      <header>
+        <p className="prelouge">Every word has a mening, find it here!</p>
+      </header>
+
+    </div>
+    </div>
+  );
+}
+
+export default App;
